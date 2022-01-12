ba70724fe23c6bc77e38f7ffd45f554a5e23b6c4
eric-c-zhang <90730607+eric-c-zhang@users.noreply.github.com>
2022-01-12 12:02:35 -0500

    make it pretty

--- resources/js/returnarray.js
+++ resources/js/returnarray.js
@@ -1,4 +1,4 @@
-const makeReturnArray = (results) => {
+export const makeReturnArray = (results) => {
     var closeArray = [];
     var returnArray = [];
     for (var p in results) {
@@ -7,7 +7,7 @@ const makeReturnArray = (results) => {
         for (var d in results[p]){
             console.log(assets[p])
             tArray.push(results[p][d].close)
-            if (d < 51) {
+            if (d < 52) {
                 let ld = 0;
                 ld = Number(d) + 1;                   //Latest date placed at start of array.
                 rArray.push((results[p][d].close-results[p][ld].close)/(results[p][ld].close))
